9b6bdfd087d0cd216f43358c6f97e2fffd21d704
Salil Debnath <114846592+salil-cloud@users.noreply.github.com>
2023-09-24 19:56:59 +0530

    Hang-Man Completed

--- Day 7/HangMan.py
+++ Day 7/HangMan.py
@@ -1,5 +1,61 @@
 import random 
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 Our_list = ["Salil", "Sharad", "Gulfam", "Rama"]
 random.shuffle(Our_list)
 Selected = random.choice(Our_list)
@@ -8,15 +64,29 @@ Selected = Selected.lower()
 display = []
 Selected_length = len(Selected)
 
+lives = 6
 for _ in range(Selected_length) :
     display += "_"
 
+end_of_game = False 
 
-User_input = input("Enter a character : ").lower()
+while not end_of_game :
+    User_input = input("Enter a character : ").lower()
 
-for i in range(Selected_length) :
-    letter = Selected[i]
-    if User_input == letter :
-        display[i] = User_input
+    for i in range(Selected_length) :
+        letter = Selected[i]
+        if User_input == letter :
+            display[i] = User_input
 
-print(display)
+    print(display)
+
+    if "_" not in display :
+        end_of_game = True
+        print("Congratulations !! You won.......")
+
+    if User_input not in Selected:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(stages[lives])
